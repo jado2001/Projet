e5ee3fbb57ddb615c397b836ab6a42b43c736636
OAMahdi <mahdi.ourahmoune@gmail.com>
2020-03-09 14:26:18 -0400

    Interaction v3
    On peut mainentenant tenir le fusil

---
+++ Projet/Assets/Scripts Classes/Bouton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouton : Objet
+{
+    override
+    public Transform interaction(GameObject destination) {
+        return transform;
+    }
+
+    override
+    public void utiliser() {
+    }
+}
---
+++ Projet/Assets/Scripts Classes/MiniObjet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniObjet: Objet
+{
+
+    override
+    public Transform interaction(GameObject destination)
+    {
+        layerObjet = transform.gameObject.layer;
+        //Ramasser l'objet
+        transform.position = destination.transform.position;
+        transform.localScale = transform.localScale*tailleRamasse; 
+        transform.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        transform.parent = destination.transform;
+        transform.localRotation = new Quaternion(0,0,0,0);
+        gameObject.layer = 11;
+        return transform;
+    }
+    override
+    public void utiliser()
+    {
+
+    }
+}
