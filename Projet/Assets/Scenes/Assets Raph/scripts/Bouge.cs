5be6a8ea47c889ea7a57e55350b09af015f0a9ab
percy1803 <raphaelgarcia1803@yahoo.com>
2020-04-06 15:58:28 -0400

    jai mis ma scene
    jai mis mon projet

---
+++ Projet/Assets/Scenes/Assets Raph/scripts/Bouge.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouge : MonoBehaviour
+{
+    public float movementSpeed = 10;
+    public float distanceTravelled = 0;
+    public Vector3 lastPosition;
+    public Vector3 startPos;
+    public bool ouvert = true;
+   // public bool ferme = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startPos = transform.position;
+        if (ouvert)
+        {
+           
+
+                transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+                distanceTravelled += Vector3.Distance(transform.position, lastPosition);
+                lastPosition = transform.position;
+                
+
+            
+            if (distanceTravelled >= 10)
+            {
+               
+                ouvert = false;
+                distanceTravelled = 0;
+            }
+            
+        }else 
+        {
+            
+                transform.Translate(-Vector3.right * movementSpeed * Time.deltaTime);
+            Debug.Log("allo");
+            distanceTravelled += Vector3.Distance(transform.position, lastPosition);
+            lastPosition = transform.position;
+
+
+            if (distanceTravelled >= 10)
+            {
+                ouvert = true;
+                distanceTravelled = 0;
+            }
+        }
+        
+    }
+}
---
+++ Projet/Assets/Scenes/Assets Raph/scripts/FoodBarHandler.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodBarHandler : MonoBehaviour
+{
+    private static Image FoodBarImage;
+
+    /// <summary>
+    /// Sets the health bar value
+    /// </summary>
+    /// <param name="value">should be between 0 to 1</param>
+    public static void SetFoodBarValue(float value)
+    {
+        FoodBarImage.fillAmount = value;
+        if (FoodBarImage.fillAmount < 0.2f)
+        {
+            SetFoodBarColor(Color.red);
+        }
+        else if (FoodBarImage.fillAmount < 0.4f)
+        {
+            SetFoodBarColor(Color.yellow);
+        }
+        else
+        {
+            SetFoodBarColor(Color.green);
+        }
+    }
+
+    public static float GetFoodBarValue()
+    {
+        return FoodBarImage.fillAmount;
+    }
+
+    /// <summary>
+    /// Sets the health bar color
+    /// </summary>
+    /// <param name="foodColor">Color </param>
+    public static void SetFoodBarColor(Color foodColor)
+    {
+        FoodBarImage.color = foodColor;
+    }
+
+    /// <summary>
+    /// Initialize the variable
+    /// </summary>
+    private void Start()
+    {
+        FoodBarImage = GetComponent<Image>();
+    }
+}
