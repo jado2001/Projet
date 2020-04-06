7e4a297bf59c58bcab085bb8e942f1e09199ed65
ArnaudLePedo <lecama2@gmail.com>
2020-04-06 15:48:28 -0400

    VaisseauMathieu

--- Projet/Assets/Standard Assets/Utility/ForcedReset.cs
+++ Projet/Assets/Standard Assets/Utility/ForcedReset.cs
@@ -2,9 +2,8 @@ using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityStandardAssets.CrossPlatformInput;
-using UnityEngine.UI;
 
-[RequireComponent(typeof (Image))]
+[RequireComponent(typeof (UnityEngine.UI.Image))]
 public class ForcedReset : MonoBehaviour
 {
     private void Update()
