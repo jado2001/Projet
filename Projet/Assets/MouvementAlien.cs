9c4abd0ba0cd8440c804df7b5afb3aa40d5d3d4a
ArnaudLePedo <lecama2@gmail.com>
2020-04-13 18:00:00 -0400

    progression alienmouvement

--- Projet/Assets/MouvementAlien.cs
+++ Projet/Assets/MouvementAlien.cs
@@ -14,6 +14,7 @@ public class MouvementAlien : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         /**if (Input.GetButtonDown("Fire1")) 
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -32,6 +33,38 @@ public class MouvementAlien : MonoBehaviour
         {
             agent.SetDestination(joueur.position);
             Debug.Log(hit.transform.name);
+=======
+       
+        Debug.DrawRay(transform.position, transform.forward*100, Color.green);
+        RaycastHit hit;
+        Vector3 vecteurDirection = joueur.position - transform.position;
+        if (Physics.Raycast(transform.position, vecteurDirection / vecteurDirection.magnitude, out hit) && hit.transform.gameObject.layer == joueur.gameObject.layer)
+        {
+            agent.SetDestination(joueur.position);
+
+            //float angle = Vector3.Angle(vecteurDirection, transform.forward);
+
+            rotation = transform.rotation.y;
+            Debug.Log(rotation);
+
+        }
+        else if (Physics.Raycast(transform.position, transform.forward, out hit) && hit.transform.gameObject.layer == 12)
+        {
+
+            Debug.Log(hit.transform.name);
+            agent.SetDestination(hit.point);
+            rotation = transform.rotation.y;
+            Debug.Log(rotation);
+        }
+        else if (agent.velocity.magnitude == 0)
+        {
+            
+            rotation = rotation + .5f;
+            transform.localRotation = Quaternion.Euler(0f, rotation, 0f);
+
+            
+            
+>>>>>>> Stashed changes
         }
         
     }
