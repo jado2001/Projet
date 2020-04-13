cbdd49c338c25b61e96380f36e79bb41a88e44b0
jado2001 <jadh2001@gmail.com>
2020-04-13 19:52:09 -0400

    modifications mineures

---
+++ Projet/Assets/Scripts/Scripts Classes/PanneauElectrique.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanneauElectrique : Objet
+{
+
+    override
+    public Transform interaction(GameObject destination)
+    {
+        
+    }
+
+
+}
