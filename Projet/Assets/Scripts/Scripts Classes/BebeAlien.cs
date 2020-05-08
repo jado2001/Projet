56f0f7099b78dd9f3a7012e3a6e2f386393a7f74
jado2001 <jadh2001@gmail.com>
2020-05-08 17:35:48 -0400

    Comportement du bébé alien

--- Projet/Assets/Scripts/Scripts Classes/BebeAlien.cs
+++ Projet/Assets/Scripts/Scripts Classes/BebeAlien.cs
@@ -1,21 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class BebeAlien : Alien
 {
-
+   
+    private Queue<GameObject> nourriture = new Queue<GameObject>();
+    private GameObject nourritureCible;
     
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
         GetComponent<Alien>().verComportement(1);
+
+		if (chercherNourriture())
+		{
+            nourritureCible = nourriture.Dequeue();
+		}
     }
+
+    public bool chercherNourriture()
+    {
+
+        foreach (GameObject objet in GetAllObjectsOnlyInScene())
+        {
+            var script = objet.GetComponent<Nourriture>();
+            if (script != null)
+            {
+                nourriture.Enqueue(objet);
+            }
+        }
+
+        if (nourriture.Count != 0)
+		{
+            return true;
+        }
+
+        return false;
+    }
+
+    public void transformerBebe()
+	{
+        transform.localScale = new Vector3(1, 1, 1);
+	}
+
+    public List<GameObject> GetAllObjectsOnlyInScene()
+    {
+        List<GameObject> objectsInScene = new List<GameObject>();
+
+        foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+        {
+            if (!EditorUtility.IsPersistent(go.transform.root.gameObject) && !(go.hideFlags == HideFlags.NotEditable || go.hideFlags == HideFlags.HideAndDontSave))
+                objectsInScene.Add(go);
+        }
+
+        return objectsInScene;
+    }
+
 }
