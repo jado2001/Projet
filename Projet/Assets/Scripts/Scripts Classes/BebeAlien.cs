1f01d0b1cb51ad0b4a64e5624398a535803bcaa8
jado2001 <jadh2001@gmail.com>
2020-05-11 16:14:47 -0400

    comment

--- Projet/Assets/Scripts/Scripts Classes/BebeAlien.cs
+++ Projet/Assets/Scripts/Scripts Classes/BebeAlien.cs
@@ -6,9 +6,9 @@ using UnityEditor;
 public class BebeAlien : Alien
 {
    
-    private Queue<GameObject> nourriture = new Queue<GameObject>();
-    private GameObject nourritureCible;
-    
+    private Queue<GameObject> nourriture = new Queue<GameObject>(); /// la liste d'objets avec le script nourriture dans la scène
+    private GameObject nourritureCible; ///l'objet que le bébé va chercher pour s'alimenter
+
 
 
     // Update is called once per frame
@@ -23,6 +23,10 @@ public class BebeAlien : Alien
 		}
     }
 
+    /// <summary>
+	/// sert à rechercher des objets dans la scène qui ont le script Nourriture
+	/// </summary>
+	/// <returns></returns> si c'est true, il y a de la nourriture dans la scène, si false, il y en a plus
     public bool chercherNourriture()
     {
 
@@ -43,11 +47,18 @@ public class BebeAlien : Alien
         return false;
     }
 
+    /// <summary>
+	/// sert à transformer le bébé alien en bébé adulte en changeant son scale
+	/// </summary>
     public void transformerBebe()
 	{
         transform.localScale = new Vector3(1, 1, 1);
 	}
 
+    /// <summary>
+	/// sert à faire une liste de tous les objets dans la scène
+	/// </summary>
+	/// <returns></returns> liste de tous les objets dans la scène
     public List<GameObject> GetAllObjectsOnlyInScene()
     {
         List<GameObject> objectsInScene = new List<GameObject>();
