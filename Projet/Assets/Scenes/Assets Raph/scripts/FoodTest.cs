74b67fb13110e1a873510da9c1f3aa719a3498ea
percy1803 <raphaelgarcia1803@yahoo.com>
2020-05-13 17:12:52 -0400

    camera
    camera

--- Projet/Assets/Scenes/Assets Raph/scripts/FoodTest.cs
+++ Projet/Assets/Scenes/Assets Raph/scripts/FoodTest.cs
@@ -13,6 +13,6 @@ public class FoodTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        FoodBarHandler.SetFoodBarValue(FoodBarHandler.GetFoodBarValue() - 0.01f);
+        //FoodBarHandler.SetFoodBarValue(FoodBarHandler.GetFoodBarValue() - 0.01f);
     }
 }
